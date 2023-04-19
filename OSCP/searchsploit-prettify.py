47e6e70ffa25ad1dbe4dc36f2d422387ca2ffaeb
dos-m0nk3y <dosm0nk3y@github.com>
2023-04-19 15:41:26 -0400

    Upload OSCP Tools

---
+++ OSCP/searchsploit-prettify.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+import json
+import subprocess
+import prettytable
+
+if len(sys.argv) < 2:
+    print(f"Usage: {sys.argv[0]} <terms>")
+    sys.exit(0)
+
+argv = " ".join(sys.argv[1:])
+result = subprocess.check_output(f"searchsploit '{argv}' -j", shell=True).decode()
+
+table = prettytable.PrettyTable(junction_char=" ")
+table.field_names = ["Exploit Title", "Path"]
+for row in [[row["Title"], row["Path"]] for row in json.loads(result)["RESULTS_EXPLOIT"]]:
+    table.add_row(row)
+
+table.align = "l"
+print(table)
